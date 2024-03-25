ab6b4909d939089af8fcae6216c49c28be4eb9d1
arfonfo <alfonsojose.sanchez@gmail.com>
2024-03-25 12:53:44 +0100

    Cambiado Back

---
+++ vue-proyecto-tfg/src/modules/Consumible/components/ConsumibleComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-5xl">{{title}}</h1>
+    <!-- foreach para mostrar los consumibles -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
---
+++ vue-proyecto-tfg/src/modules/Consumible/views/ConsumibleView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ConsumibleComponent :title=title />
+  </div>
+</template>
+
+<script>
+  import { defineAsyncComponent } from 'vue'
+  import axios from 'axios'
+  
+  export default {
+    data() {
+      return {
+        title: 'Consumibles',
+        consumibles: null
+      }
+    },
+    components: {
+      ConsumibleComponent: defineAsyncComponent(() => import('@/modules/Consumible/components/ConsumibleComponent.vue'))
+    },
+    mounted() {
+      axios
+        .get('')
+        .then(response => (this.consumibles = response.json()))
+    }
+  }
+
+</script>
