db402d99e6ee8cee712491d5834bb5897009ea48
arfonfo <alfonsojose.sanchez@gmail.com>
2024-04-04 11:41:39 +0200

    Mo

---
+++ vue-proyecto-tfg/src/modules/Consumible/components/ConsumiblesComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div>
+      <MensajesComponent :message="getMensaje" :type="getTipo"
+      :mostrarMensaje="getMostrar"
+      />
+    </div>
+    <TablaComponent :tableHead="getConsumibles" :tableBody="getConsumibles" />
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  data() {
+    return {
+      showModal: false
+    }
+  },
+  computed: {
+    ...mapGetters('Consumible', ['getConsumibles']),
+    ...mapGetters('Shared', ['getTipo', 'getMensaje', 'getMostrar'])
+  },
+  components: {
+    MensajesComponent: defineAsyncComponent(() => import('@/modules/shared/components/MensajesComponent.vue')),
+    //EditConsumibleComponent: defineAsyncComponent(() => import('@/modules/Consumible/components/EditConsumibleComponent.vue')),
+    //ModalComponent: defineAsyncComponent(() => import('@/modules/shared/components/ModalComponent.vue')),
+    TablaComponent: defineAsyncComponent(() => import('@/modules/shared/components/TablaComponent.vue'))
+
+  },      
+  watch: {
+    getConsumibles() {
+      this.fillTableBody()
+    }
+  },
+  methods: {
+    async fillTableBody() {
+      this.tableBody = await this.getConsumibles.map((consumible) => ({
+        nombre: consumible.nombre,
+        precio: consumible.precio,
+      }))
+    },
+  }
+
+}
+</script>
