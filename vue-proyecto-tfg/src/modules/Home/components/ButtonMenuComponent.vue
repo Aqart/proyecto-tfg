8b53e30dca51eeabf717b94a532a1c75f07f046d
Aqart <felipe.karavaka@gmail.com>
2024-04-16 18:16:57 +0200

    cambios responsive

--- vue-proyecto-tfg/src/modules/Home/components/ButtonMenuComponent.vue
+++ vue-proyecto-tfg/src/modules/Home/components/ButtonMenuComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link
     :to="`/${path.toLowerCase()}`"
-    class="inline-flex items-center justify-center bg-stone font-bold text-secondary rounded-lg border-secondary hover:ring-2 hover:ring-secondary hover:ring-offset-2 w-screen sm:w-full h-48 text-center text-2xl sm:text-sm md:text-base"
+    class="flex flex-row sm:items-center sm:justify-center items-center justify-center bg-stone font-bold text-secondary rounded-lg border-secondary hover:ring-2 hover:ring-secondary hover:ring-offset-2 w-4/5 sm:w-full sm:h-96 h-24 text-center text-2xl sm:text-sm md:text-base"
   >
     <FontAwesomeIcon v-if="icon" :icon="icon" class="mr-2" />
     {{ name }}
