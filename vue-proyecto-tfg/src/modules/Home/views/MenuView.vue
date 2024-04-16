451ce3fad29cc432f24bbf6ccb00250dcd3ae979
Aqart <felipe.karavaka@gmail.com>
2024-04-16 18:16:57 +0200

    cambios responsive

--- vue-proyecto-tfg/src/modules/Home/views/MenuView.vue
+++ vue-proyecto-tfg/src/modules/Home/views/MenuView.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="space-y-4">
-    <div class="grid grid-cols-1 sm:grid-cols-2 gap-3">
+    <div
+      class="flex flex-col sm:grid sm:grid-cols-2 gap-3 justify-center sm:justify-start items-center sm:items-start"
+    >
       <ButtonMenuComponent name="Calcular Costes" path="calculadora" class="w-full p-3" />
       <ButtonMenuComponent name="Controles horarios" path="controles-horarios" class="w-full p-3" />
     </div>
-    <div class="grid grid-cols-1 sm:grid-cols-3 gap-3">
+    <div
+      class="flex flex-col sm:grid sm:grid-cols-3 gap-3 justify-center sm:justify-start items-center sm:items-start"
+    >
       <ButtonMenuComponent name="Consumibles" path="consumibles" class="w-full p-3" />
       <ButtonMenuComponent name="Gastos-Generales" path="gastos-generales" class="w-full p-3" />
       <ButtonMenuComponent name="Maquinas" path="maquinas" class="w-full p-3" />
