9d81e377cc49ccc9967622e04a6c20eb4d45708c
unknown <juanant.guirao@gmail.com>
2024-04-18 20:26:59 +0200

    [Add] Añadidos iconos

--- vue-proyecto-tfg/src/modules/Home/views/MenuView.vue
+++ vue-proyecto-tfg/src/modules/Home/views/MenuView.vue
@@ -7,19 +7,52 @@
         name="Calcular Costes"
         path="calculadora"
         class="w-full p-3"
-        :icon="['fas', 'map']"
+        :icon="['fas', 'fa-calculator']"
       />
-      <ButtonMenuComponent name="Controles horarios" path="controles-horarios" class="w-full p-3" />
+      <ButtonMenuComponent
+       name="Controles horarios"
+       path="controles-horarios"
+       class="w-full p-3"
+       :icon="['fas', 'fa-list-check']"
+          />
     </div>
     <div
       class="flex flex-col sm:grid sm:grid-cols-3 gap-3 justify-center sm:justify-start items-center sm:items-start"
     >
-      <ButtonMenuComponent name="Consumibles" path="consumibles" class="w-full p-3" />
-      <ButtonMenuComponent name="Gastos-Generales" path="gastos-generales" class="w-full p-3" />
-      <ButtonMenuComponent name="Maquinas" path="maquinas" class="w-full p-3" />
-      <ButtonMenuComponent name="Transportes" path="Transportes" class="w-full p-3" />
-      <ButtonMenuComponent name="Trabajadores" path="trabajadores" class="w-full p-3" />
-      <ButtonMenuComponent name="Usuarios" path="users" class="w-full p-3" />
+      <ButtonMenuComponent 
+      name="Consumibles" 
+      path="consumibles" 
+      class="w-full p-3" 
+      :icon="['fas', 'fa-droplet']"
+      />
+      <ButtonMenuComponent
+        name="Gastos-Generales"
+        path="gastos-generales"
+        class="w-full p-3"
+        :icon="['fas', 'fa-bolt']"
+          />
+      <ButtonMenuComponent
+        name="Maquinas"
+        path="maquinas"
+        class="w-full p-3"
+        :icon="['fas', 'fa-oil-well']"  />
+      <ButtonMenuComponent
+       name="Transportes" 
+       path="Transportes" 
+       class="w-full p-3" 
+       :icon="['fas', 'fa-truck']"/>
+      <ButtonMenuComponent
+       name="Trabajadores"
+        path="trabajadores" 
+        class="w-full p-3" 
+        :icon="['fas', 'fa-person-digging']"
+        />
+      <ButtonMenuComponent
+       name="Usuarios" 
+       path="users" 
+       class="w-full p-3"
+       :icon="['fas', 'fa-users']" 
+       />
     </div>
   </div>
 </template>
