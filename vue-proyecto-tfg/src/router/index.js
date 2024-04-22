94c78ed149277dedcb6d9b0a7cbc79df42e1ecfc
Aqart <felipe.karavaka@gmail.com>
2024-04-22 10:12:38 +0200

    Arreglada fuente Lexend y router para hacer build

--- vue-proyecto-tfg/src/router/index.js
+++ vue-proyecto-tfg/src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeRouter from '@/modules/Home/router'
 import AuthRouter from '@/modules/Auth/router'
 import store from '@/store'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/home',
