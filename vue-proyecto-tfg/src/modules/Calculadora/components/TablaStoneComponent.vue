1a474078657ea8de49b9d6d6c45d4451aa30ef03
arfonfo <alfonsojose.sanchez@gmail.com>
2024-05-03 21:27:44 +0200

    Cambios calculadoraComponent

--- vue-proyecto-tfg/src/modules/Calculadora/components/TablaStoneComponent.vue
+++ vue-proyecto-tfg/src/modules/Calculadora/components/TablaStoneComponent.vue
@@ -3,12 +3,11 @@
     <h1>Calcular tabla</h1>
     <form @submit.prevent="handleSubmit" class="flex flex-col gap-3">
       <InputNumberComponent
-        label="Número 1"
-        placeholder="Introduce un número"
+        label="Grosor de la Tabla"
+        placeholder="el grosor en centímetros"
         @changeNumber="handleChange"
         @errorNumber="handleError"
       />
-
       <SelectMaquinaComponent
         :maquinasSeleccionadas="maquinas"
         :options="getMaquinas"
@@ -16,19 +15,76 @@
         @removeMaquina="removeMaquinasArray"
       />
 
-      <label for="maquina">Seleccione máquina</label>
-      <select name="maquina" ref="maquina">
-        <option v-for="maquina in getMaquinas" :key="maquina.id" :value="maquina.id">
-          {{ maquina.nombre }}
-        </option>
-      </select>
-      <label for="terminacion">Seleccione terminación</label>
-      <select name="terminacion" ref="terminacion">
-        <option value="10">Solo cortado</option>
-        <option value="20">Apomazado</option>
-        <option value="30">Envejecido</option>
-        <option value="40">Abujardado</option>
-      </select>
+
+
+      <label for="terminacion"
+        class="block mb-2 text-xl font-medium first-letter:uppercase text-shadow text-stoneBackground-3"
+      >
+        Terminación
+      </label>
+      <ul class="items-center w-full text-sm text-secondary bg-stoneBackground-2 bg-opacity-50 border border-stone border-opacity-50 rounded-lg sm:flex">
+          <li class="w-full border-b border-stone border-opacity-50 sm:border-b-0 sm:border-r">
+              <div class="flex items-center ps-3">
+                  <input id="soloCortado" type="radio" value="10" name="terminacion" class="accent-stoneBackground-5 w-4 h-4 text-stone bg-stone border-stoneBackground-5">
+                  <label for="soloCortado" class="w-full py-3 ms-2 text-sm font-medium text-secondary">Solo cortado</label>
+              </div>
+          </li>
+          <li class="w-full border-b border-stone border-opacity-50 sm:border-b-0 sm:border-r">
+              <div class="flex items-center ps-3">
+                  <input id="apomazado" type="radio" value="20" name="terminacion" class="accent-stoneBackground-5 w-4 h-4 text-stone bg-stone border-stoneBackground-3">
+                  <label for="apomazado" class="w-full py-3 ms-2 text-sm font-medium text-secondary">Apomazado</label>
+              </div>
+          </li>
+          <li class="w-full border-b border-stone border-opacity-50 sm:border-b-0 sm:border-r">
+              <div class="flex items-center ps-3">
+                  <input id="envejecido" type="radio" value="30" name="terminacion" class="accent-stoneBackground-5 w-4 h-4 text-stone bg-stone border-stoneBackground-3">
+                  <label for="envejecido" class="w-full py-3 ms-2 text-sm font-medium text-secondary">Envejecido</label>
+              </div>
+          </li>
+          <li class="w-full">
+              <div class="flex items-center ps-3">
+                  <input id="abujardado" type="radio" value="40" name="terminacion" class="accent-stoneBackground-5 w-4 h-4 text-stone bg-stone border-stoneBackground-3">
+                  <label for="abujardado" class="w-full py-3 ms-2 text-sm text-seconsary">Abujardado</label>
+              </div>
+          </li>
+      </ul>
+
+      <label for="embalaje"
+        class="block mb-2 text-xl font-medium first-letter:uppercase text-shadow text-stoneBackground-3"
+      >
+        Embalaje
+      </label>
+    <ul class="grid w-full gap-6 md:grid-cols-2">
+        <li>
+          <input type="radio" id="embalado" name="embalaje" value="20" class="hidden peer" required />
+          <label for="embalado" c
+              class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100">                           
+              <div class="block">
+                  <div class="w-full text-lg font-semibold">Embalado</div>
+                  <div class="w-full text-sm">Conlleva cargo extra</div>
+              </div>
+              <!-- En lugar de la flecha incluir un icono de FontAwesome -->
+              <svg class="w-5 h-5 ms-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+              </svg>
+          </label>
+        </li>
+        <li>
+            <input type="radio" id="hosting-big" name="embalaje" value="hosting-big" class="hidden peer">
+            <label for="hosting-big" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100">
+                <div class="block">
+                    <div class="w-full text-lg font-semibold">No embalado</div>
+                    <div class="w-full text-sm">No conlleva cargo extra</div>
+                </div>
+                <!-- En lugar de la flecha incluir un icono de FontAwesome -->
+                <svg class="w-5 h-5 ms-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </label>
+        </li>
+    </ul>
+
+
 
       <label for="embalaje">Seleccione embalaje</label>
       <select name="embalaje" ref="embalaje">
