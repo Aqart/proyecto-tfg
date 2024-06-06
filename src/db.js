a716a572c50ecdd743471d6702488b2af8ff8e99
Aqart <felipe.karavaka@gmail.com>
2024-06-06 03:38:12 +0200

    cambiada configuración a variables de entorno

--- src/db.js
+++ src/db.js
@@ -4,20 +4,14 @@ const dotenv = require('dotenv') // Import the dotenv module to load environment
 dotenv.config() // Load environment variables from the .env file
 
 //Configuration for the database connection
+
 const dbConfig = {
-    host: '217.61.210.26',
-    port: 3306,
-    user: 'udb_api',
-    password: 'z88l7Sh9!',
-    database: 'transforstone_app',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 }
-// const dbConfig = {
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE,
-// }
 
 // Create the connection pool
 const pool = mysql.createPool(dbConfig)
